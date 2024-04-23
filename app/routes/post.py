25eb6921e11397e70219cff376519db12df7aae0
abhianv <abhianvkallungal15@gmail.com>
2024-04-23 21:00:08 +0530

    Add the ability to search for content in tweets (match against the beginning of the tweet only)

--- app/routes/post.py
+++ app/routes/post.py
@@ -1,13 +1,16 @@
 
 
-from fastapi import APIRouter, Request, UploadFile, File,Form,HTTPException
+from fastapi import APIRouter, Request, UploadFile, File,Form,HTTPException,Query
 from fastapi.responses import JSONResponse
 from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore, auth, storage
+from fastapi import Request,HTTPException,Depends
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
 
 db = firestore.client()
@@ -58,3 +61,36 @@ async def create_tweet(request: Request,tweet: str = Form(...), image: UploadFil
  
 
     return {"message": "Tweet created successfully"}
+
+
+@router.get("/tweets")
+async def search_tweets(request: Request,query: str = Query(None)):
+    try:
+        if query:
+            tweets_ref = db.collection("tweets")
+            tweet_docs = tweets_ref.stream()
+
+            # Extract tweet data from documents
+            tweets = []
+            for doc in tweet_docs:
+                tweet_data = doc.to_dict()
+                if tweet_data["tweet"].startswith(query):  # Filter results to match beginning of content
+                    tweets.append(tweet_data)
+
+            return templates.TemplateResponse("tweet.html", {"request": request,"tweets": tweets })
+        else:
+            # If no search query, return all tweets in descending order
+            tweets_ref = db.collection("tweets")
+            tweet_docs = tweets_ref.stream()
+
+            # Extract tweet data from documents
+            tweets = []
+            for doc in tweet_docs:
+                tweet_data = doc.to_dict()
+                tweets.append(tweet_data)
+
+            return templates.TemplateResponse("tweet.html", {"request": request,"tweets": tweets })
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Internal server error")
